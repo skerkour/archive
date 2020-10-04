6b10b9ac2a51441abfa752366004724ddbde84e7
skerkour <sylvain@kerkour.fr>
2020-10-04 21:30:15 +0200

    Add bloom_templates2

---
+++ bloom_templates2/vue/Component.vue
@@ -0,0 +1,26 @@
+<template>
+</template>
+
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+
+@Component
+export default class Component extends Vue {
+  // props
+  // data
+  // computed
+  // lifecycle
+  // watch
+  // methods
+}
+
+//@Prop({ type: String, default: 'right' }) x!: string;
+//@Watch('pagination', { deep: true })
+
+</script>
+
+
+<style scoped lang="scss">
+</style>
